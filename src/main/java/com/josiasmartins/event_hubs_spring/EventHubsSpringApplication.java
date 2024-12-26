2342ee077893ec4d341015a495ebe478480f588c
Josias Martins <josiasmartinscaitano@gmail.com>
2024-12-26 15:50:44 -0300

    feature: initial project

---
+++ src/main/java/com/josiasmartins/event_hubs_spring/EventHubsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.josiasmartins.event_hubs_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	EventHubsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHubsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josiasmartins/event_hubs_spring/EventHubsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josiasmartins.event_hubs_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
